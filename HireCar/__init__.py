651ac983fb54a1091bb19036182150068fa59836
H. M. Tahsin Sheikh <tahsinsheikh10039@gmail.com>
2025-09-16 22:28:15 +0600

    Initial commit

---
+++ HireCar/__init__.py
@@ -0,0 +1,5 @@
+# This file marks the HireCar directory as a Python package.
+# You can add any package-level imports or initializations here.
+
+# Example: Importing submodules or setting up package-level variables
+# from . import some_module
