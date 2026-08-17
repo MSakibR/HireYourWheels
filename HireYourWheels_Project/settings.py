49e4eecb92cbdfe7a55083e1429a6d837a51eabd
Sakib Rahman <139787057+MSakibR@users.noreply.github.com>
2026-08-18 01:16:08 +0600

    Update settings.py

--- HireYourWheels_Project/settings.py
+++ HireYourWheels_Project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8t^_s!og4^u2()a%yie189m9-4v&k2h_wcc6_h8g7fl%cilpjj'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
