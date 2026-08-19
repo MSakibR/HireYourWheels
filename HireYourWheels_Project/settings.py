3cd94237f7f80037d4932ac7eed39c3e99fbe389
Sakib Rahman <139787057+MSakibR@users.noreply.github.com>
2026-08-19 16:04:17 +0600

    Update settings.py

--- HireYourWheels_Project/settings.py
+++ HireYourWheels_Project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 #import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -85,11 +86,17 @@ WSGI_APPLICATION = 'HireYourWheels_Project.wsgi.application'
 #BASE_DIR / 'db.sqlite3
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/tmp/db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
+
+if 'DIRECT_URL' in os.environ and os.environ.get('DIRECT_URL'):
+    DATABASES['default']['HOST'] = dj_database_url.parse(os.environ.get('DIRECT_URL'))['HOST']
+    DATABASES['default']['PORT'] = dj_database_url.parse(os.environ.get('DIRECT_URL'))['PORT']
 
 
 # Password validation
