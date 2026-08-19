19613479bc2f639be3df4e90742b54505449634b
Sakib Rahman <139787057+MSakibR@users.noreply.github.com>
2026-08-19 15:26:01 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,12 @@
 import os
 import sys
 
-# প্রজেক্টের রুট ডিরেক্টরিকে Python Path-এ যুক্ত করা
+# Current directory & parent directory path setup
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HireYourWheels_Project.settings')
+
+from django.core.wsgi import get_wsgi_application
 
 app = get_wsgi_application()
