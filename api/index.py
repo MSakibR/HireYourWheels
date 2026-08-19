d1362270e617e09113dbf408f667783354156461
Sakib Rahman <139787057+MSakibR@users.noreply.github.com>
2026-08-19 15:22:51 +0600

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,11 @@
 import os
+import sys
 
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "HireYourWheels_Project.settings"
-)
+# প্রজেক্টের রুট ডিরেক্টরিকে Python Path-এ যুক্ত করা
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HireYourWheels_Project.settings')
 
 app = get_wsgi_application()
